6dd82cad28476d3e939e4ba0e4948f905a785939
niki06roxana-afk <niki06roxana@gmail.com>
2026-04-24 09:49:29 -0500

    cambio base en main

--- app.py
+++ app.py
@@ -1,13 +1,2 @@
 
-from curses import echo
-print("Hola Mundo")
-print("Hola Mundo 2")
-print("Hola Mundo 3")
-print("rama2 - login de usuarios")
-
-
-print("main - sistema base")
-print("rama1 -registro de usuarios")
-print("rama2 - login de usuarios")
-print("rama3 - panel administrativo")
-print("rama3 - panel administrativo")
+print("main -m sistema base modificado")
